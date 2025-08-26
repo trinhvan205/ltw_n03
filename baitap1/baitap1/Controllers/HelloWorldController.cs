4fcfda4c97ee344bc5440d2235f91227658218be
trinhvan205 <tranvantrinh1301@gmail.com>
2025-08-26 21:43:24 +0700

    thêm bài lập trình web 22/8

---
+++ baitap1/baitap1/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace baitap1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
