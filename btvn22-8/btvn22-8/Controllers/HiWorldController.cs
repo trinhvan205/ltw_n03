1e20f34f4e220d9b486be4cffd8c151d623688cc
trinhvan205 <tranvantrinh1301@gmail.com>
2025-08-26 22:42:21 +0700

    Add folder btvn22-8

---
+++ btvn22-8/btvn22-8/Controllers/HiWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace btvn22_8.Controllers
+{
+    public class HiWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ btvn22-8/btvn22-8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace btvn22_8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
