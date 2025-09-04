9587a4a435515b6fc5a7bf9c7ed511fe9de7ef27
trinhvan205 <tranvantrinh1301@gmail.com>
2025-09-05 01:01:44 +0700

    Thêm NetCoreMVCLab03

---
+++ NetCoreMVCLab03/NetCoreMVCLab03/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreMVCLab03.Models;
+namespace NetCoreMVCLab03.Controllers
+{
+    public class BookController : Controller
+    {
+        protected Book book = new Book();
+        public IActionResult Index()
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            var books = book.GetBookList();
+            return View(books);
+        }
+        public IActionResult Create()
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            Book model = new Book();
+            return View(model);
+        }
+        public IActionResult Edit(int id)
+        {
+            ViewBag.authors = book.Authors;
+            ViewBag.genres = book.Genres;
+            Book model = book.GetBookById(id);
+            return View(model);
+        }
+    }
+}
---
+++ NetCoreMVCLab03/NetCoreMVCLab03/Models/Book.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace NetCoreMVCLab03.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int AuthorId { get; set; }
+        public int GenreId { get; set; }
+        public string Image { get; set; }
+        public float Price { get; set; }
+        public int TotalPage { get; set; }
+        public string Summary { get; set; }
+
+        // danh sach cac cuon sach
+        public List<Book> GetBookList()
+        {
+            List<Book> books = new List<Book>()
+            {
+                new Book() {
+                    Id = 1,
+                    Title = "Chí Phèo",
+                    AuthorId = 1,
+                    GenreId = 1,
+                    Image = "/images/products/b1.jpg",
+                    Price = 12000,
+                    TotalPage = 120,
+                },
+            };
+
+            return books;
+        }
+        public Book GetBookById(int id)
+        {
+            Book book = this.GetBookList().FirstOrDefault(b => b.Id == id);
+            return book;
+        }
+        public List<SelectListItem> Authors { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Nam Cao" },
+
+
+        };
+        public List<SelectListItem> Genres { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "1", Text = "Văn học Việt Nam" },
+        };
+    }
+}
