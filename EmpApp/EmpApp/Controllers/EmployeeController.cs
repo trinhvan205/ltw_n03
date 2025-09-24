028de15f2a30591daee8a326d6fc68ad6119ba8b
trinhvan205 <tranvantrinh1301@gmail.com>
2025-09-24 18:16:17 +0700

    add EmpApp

---
+++ EmpApp/EmpApp/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmpApp.Models;
+namespace EmpApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult empList()
+        {
+            using (EmployeeEntities db = new EmployeeEntities())
+            {
+                var emplist = db.Employees.ToList();
+
+                return View(emplist);
+            }
+        }
+    }
+}
