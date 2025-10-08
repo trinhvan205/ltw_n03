28aee3b67532ddff1fcf2e98cea4d902e7bc374d
trinhvan205 <tranvantrinh1301@gmail.com>
2025-10-08 20:05:33 +0700

    Data access CodeFirst Day08

---
+++ TvTDay08EFCF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TvTDay08EFCF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ TvTDay08EFCF/Models/K64CNT1DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TvTDay08EFCF.Models
+{
+    public class K64CNT1DbContext:DbContext
+    {
+        public K64CNT1DbContext(DbContextOptions<K64CNT1DbContext> options):
+            base(options)
+        { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
